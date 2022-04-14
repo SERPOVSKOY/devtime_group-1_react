f50240ace7d1798ee8b930505a6fbbdde801c633
LENOVO <ksorkin08@gmail.com>
2022-04-14 18:52:11 +0300

    babel loader added

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,19 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     mode: 'development',
-    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
+    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
 }
